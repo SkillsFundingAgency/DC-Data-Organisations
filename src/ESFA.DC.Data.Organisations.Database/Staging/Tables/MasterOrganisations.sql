218243e6700460b539055abbdcec59fa524a17bd
Peter Chamley <peter@chamley.name>
2018-08-15 15:41:42 +0100

    Update SP to Process MasterOrg, Add Partner SP to Process SP.

--- src/ESFA.DC.Data.Organisations.Database/Staging/Tables/MasterOrganisations.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [Staging].[MasterOrganisations] (
-    [UKPRN] BIGINT NOT NULL,
-    CONSTRAINT [PK_Staging_MasterOrganisations] PRIMARY KEY CLUSTERED ([UKPRN] ASC)
-);
-
-GO
-GRANT ALTER ON OBJECT::Staging.MasterOrganisations TO [Org_RW_User];
-
