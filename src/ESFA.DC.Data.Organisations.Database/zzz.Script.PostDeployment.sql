bf39c0fc5576f56a71cce4c3a729e7783d4c7cb1
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-11-09 13:42:02 +0000

    Change DisplayDeploymentProperties from View to SP

--- src/ESFA.DC.Data.Organisations.Database/zzz.Script.PostDeployment.sql
+++ src/ESFA.DC.Data.Organisations.Database/zzz.Script.PostDeployment.sql
@@ -43,64 +43,8 @@ IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('DeploymentDateti
 ELSE
 	EXEC sp_updateextendedproperty @name = N'DeploymentDatetime', @value = @DeploymentTime;  
 
-
 GO
-PRINT N'   Extended Property : Complete.';
-
-GO
-RAISERROR('  Update User Account Passwords',10,1) WITH NOWAIT;
-/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
-
-SET NOCOUNT ON;
-GO
-
-RAISERROR('		   Extended Property',10,1) WITH NOWAIT;
-GO
-
-RAISERROR('		         %s - %s',10,1,'BuildNumber','$(BUILD_BUILDNUMBER)') WITH NOWAIT;
-IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('BuildNumber', default, default, default, default, default, default))
-	EXEC sp_addextendedproperty @name = N'BuildNumber', @value = '$(BUILD_BUILDNUMBER)';  
-ELSE
-	EXEC sp_updateextendedproperty @name = N'BuildNumber', @value = '$(BUILD_BUILDNUMBER)';  
-	
-GO
-RAISERROR('		         %s - %s',10,1,'BuildBranch','$(BUILD_BRANCHNAME)') WITH NOWAIT;
-IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('BuildBranch', default, default, default, default, default, default))
-	EXEC sp_addextendedproperty @name = N'BuildBranch', @value = '$(BUILD_BRANCHNAME)';  
-ELSE
-	EXEC sp_updateextendedproperty @name = N'BuildBranch', @value = '$(BUILD_BRANCHNAME)';  
-
-GO
-DECLARE @DeploymentTime VARCHAR(35) = CONVERT(VARCHAR(35),GETUTCDATE(),113);
-RAISERROR('		         %s - %s',10,1,'DeploymentDatetime',@DeploymentTime) WITH NOWAIT;
-IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('DeploymentDatetime', default, default, default, default, default, default))
-	EXEC sp_addextendedproperty @name = N'DeploymentDatetime', @value = @DeploymentTime;  
-ELSE
-	EXEC sp_updateextendedproperty @name = N'DeploymentDatetime', @value = @DeploymentTime;  
-GO
-
-IF EXISTS (SELECT * FROM [sys].[objects] WHERE [type] = 'V' AND Name = 'DisplayDeploymentProperties_VW')
-BEGIN 
-	DROP VIEW [dbo].[DisplayDeploymentProperties_VW];
-END
-
-GO
-EXEC ('CREATE VIEW [dbo].[DisplayDeploymentProperties_VW]
-AS
-	SELECT name, value 
-	FROM fn_listextendedproperty(default, default, default, default, default, default, default);  
-	');
-
+DROP VIEW IF EXISTS [dbo].[DisplayDeploymentProperties_VW];
 GO
 
 RAISERROR('		   Update User Account Passwords',10,1) WITH NOWAIT;
